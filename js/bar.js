f08f9e98aab1bae691a9931d763526d6b3123685
Tim Bodyka Heng <nomad.kaka@live.com>
2016-04-05 15:26:12 -0700

    allow dragging

--- js/bar.js
+++ js/bar.js
@@ -1,6 +1,39 @@
 var w = 1500;
 var h = 900;
 var barPadding = 1;
+
+// var drag = d3.behavior.drag()
+//     .origin(function(d){
+//         return d;
+//     })
+//     .on("dragstart", dragstarted)
+//     .on("drag", dragged)
+//     .on("dragend", dragended);
+
+var drag = d3.behavior.drag()
+    .on("drag", function(d, i) {
+        d.x += d3.event.dx
+        d.y += d3.event.dy
+        d3.select(this).attr("transform", function(d, i) {
+            return "translate(" + [d.x, d.y] + ")";
+        })
+    });
+
+function dragstarted(d) {
+    d3.event.sourceEvent.stopPropagation();
+    d3.select(this).classed("dragging", true);
+};
+
+function dragged(d) {
+    console.log(d.x);
+    d3.select(this).attr("cx", d.x = d3.event.x)
+        .attr("cy", d.y = d3.event.y);
+};
+
+function dragended(d) {
+    d3.select(this).classed("dragging", false);
+};
+
 var bars = function(data) {
     // max = d3.max(data, function(d) {
     //     return d.value;
@@ -37,7 +70,7 @@ var bars = function(data) {
     //     })
     //     .attr("width", 20)
     //     .attr("height", h);
-    var svg = d3.select("#svg")
+    var svg = d3.select("#canvas")
         .attr("width", w)
         .attr("height", h);
     svg.selectAll("rect")
@@ -60,11 +93,12 @@ var bars = function(data) {
         })
         .attr("title", function(d) {
             return d.operator + ", " + d.fatalities + " (" + d.year.getFullYear() + ")";
-        });
+        })
+        .call(drag);
 };
 
 function init() {
-    var svg = d3.select("#svg")
+    var svg = d3.select("#canvas")
         .attr("width", w + 100)
         .attr("height", h + 100);
     svg.append("svg:rect")
@@ -75,24 +109,61 @@ function init() {
 };
 
 function visualizeNodeGraph(data) {
-    var svg = d3.select("#svg")
+    var svg = d3.select("#canvas")
         .attr("width", w)
         .attr("height", h);
     svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
+        .filter(function(d) {
+            return d.fatalities > 100;
+        })
         .attr("class", "node")
         .attr("cx", function(d) {
-            return Math.floor((Math.random() * w) + 1);
+            var posX = Math.floor((Math.random() * w) + 1);
+            // var posX = +d.year.getFullYear();
+            return d.x = posX;
         })
         .attr("cy", function(d) {
-            return Math.floor((Math.random() * h) + 1);
+            var posY = Math.floor((Math.random() * h) + 1);
+            // var posY = +d.fatalities;
+            return d.y = posY;
         })
         .attr("r", function(d) {
             return d.fatalities / 5;
         })
         .attr("title", function(d) {
             return d.operator + ", " + d.fatalities + " (" + d.year.getFullYear() + ")";
-        });
+        })
+        .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        })
+        .call(drag);
 };
+
+// function nodeConstructor(svg, data, x, y) {
+//     svg.selectAll("circle")
+//         .data(data)
+//         .enter()
+//         .append("circle")
+//         .attr("class", "node")
+//         .attr("cx", function(d) {
+//             var posX = Math.floor((Math.random() * w) + 1);
+//             return d.x = posX;
+//         })
+//         .attr("cy", function(d) {
+//             var posY = Math.floor((Math.random() * h) + 1);
+//             return d.y = posY;
+//         })
+//         .attr("r", function(d) {
+//             return d.fatalities / 5;
+//         })
+//         .attr("title", function(d) {
+//             return d.operator + ", " + d.fatalities + " (" + d.year.getFullYear() + ")";
+//         })
+//         .attr("transform", function(d) {
+//             return "translate(" + d.x + "," + d.y + ")";
+//         })
+//         .call(drag);
+// };
