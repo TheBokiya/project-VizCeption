574a6a56ccab14bef7e9c188625ef1e3e81430ac
Tim Bodyka Heng <nomad.kaka@live.com>
2016-04-13 17:28:31 -0700

    fixes issues after branching where states not loaded properly

--- js/bar.js
+++ js/bar.js
@@ -8,7 +8,9 @@ var timer = 5000;
 
 var progress = [];
 var progressNodeMetaData = [];
-var currentStatePos = { "x": 40, "y": progressH - 50 };
+var savedGraph = [];
+
+var currentStatePos = { "x": 40, "y": progressH };
 
 var timeout = null;
 
@@ -62,7 +64,12 @@ function saveState() {
     // console.log(node[0][0].getAttribute('cx'));
     progress.push(savedNode);
     currentStatePos.y -= 40;
-    progressNodeMetaData.push({ "x": currentStatePos.x, "y": currentStatePos.y, "type": "state" });
+    progressNodeMetaData.push({
+        "x": currentStatePos.x,
+        "y": currentStatePos.y,
+        "type": "state",
+        "id": progress.length
+    });
     d3.select("#progress").selectAll("*").remove();
     progressView(progressNodeMetaData);
 
@@ -71,35 +78,6 @@ function saveState() {
     //     document.getElementById("state-" + i).addEventListener("click", testClick(i));
     // }
 }
-
-
-// var bars = function(data) {
-//     var svg = d3.select("#canvas")
-//         .attr("width", w)
-//         .attr("height", h);
-//     svg.selectAll("rect")
-//         .data(data)
-//         .enter()
-//         .append("rect")
-//         // .filter(function(d){
-//         //  return d.operator == "Air Canada";
-//         // })
-//         .attr("class", "bar")
-//         .attr("x", function(d, i) {
-//             return i * (w / data.length);
-//         })
-//         .attr("y", function(d) {
-//             return h - (d.fatalities);
-//         })
-//         .attr("width", 3)
-//         .attr("height", function(d) {
-//             return d.fatalities;
-//         })
-//         .attr("title", function(d) {
-//             return d.operator + ", " + d.fatalities + " (" + d.year.getFullYear() + ")";
-//         });
-// };
-var savedGraph = [];
 
 function progressView(data) {
     // currentStatePos.y -= 40;
@@ -140,7 +118,10 @@ function progressView(data) {
                 return "branch-node";
         })
         .attr("id", function(d, i) {
-            return i;
+            if (d.type === "state")
+                return d.id;
+            else 
+                return "branch-" + i;
         })
         .attr("cx", function(d) {
             return d.x;
@@ -150,7 +131,7 @@ function progressView(data) {
         })
         .attr("r", function(d) {
             if (d.type === "state")
-                return 11;
+                return 10;
             else
                 return 5;
         })
@@ -162,7 +143,9 @@ function progressView(data) {
         });
     $("#progress > circle.progress-node").click(function() {
         var index = $(this).attr("id");
-        loadSavedState(progress[index]);
+        $('.selected-state').removeClass('selected-state');
+        $(this).addClass('selected-state');
+        loadSavedState(progress[index-1]);
     });
 }
 
@@ -211,7 +194,7 @@ function visualizeNodeGraph(data) {
 };
 
 function loadSavedState(state) {
-    console.log(state);
+    console.log(progress);
     d3.select("#canvas").selectAll("*").remove();
     var svg = d3.select("#canvas");
     node = svg.selectAll("circle")
@@ -234,29 +217,3 @@ function loadSavedState(state) {
         .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
         .call(drag);
 }
-
-// function nodeConstructor(svg, data, x, y) {
-//     svg.selectAll("circle")
-//         .data(data)
-//         .enter()
-//         .append("circle")
-//         .attr("class", "node")
-//         .attr("cx", function(d) {
-//             var posX = Math.floor((Math.random() * w) + 1);
-//             return d.x = posX;
-//         })
-//         .attr("cy", function(d) {
-//             var posY = Math.floor((Math.random() * h) + 1);
-//             return d.y = posY;
-//         })
-//         .attr("r", function(d) {
-//             return d.fatalities / 5;
-//         })
-//         .attr("title", function(d) {
-//             return d.operator + ", " + d.fatalities + " (" + d.year.getFullYear() + ")";
-//         })
-//         .attr("transform", function(d) {
-//             return "translate(" + d.x + "," + d.y + ")";
-//         })
-//         .call(drag);
-// };
